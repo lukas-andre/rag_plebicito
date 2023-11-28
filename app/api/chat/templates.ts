8ff0f9fe3c141d2235dbcc3e37f7637a7aef17d1
Lucas Henry <lhenry@falabella.cl>
2023-11-28 00:06:40 -0300

    feat: improve prompt template

--- app/api/chat/templates.ts
+++ app/api/chat/templates.ts
@@ -1,22 +1,21 @@
 const templates = {
   plebicitoTemplate: `
     Filtrado de Mensajes:
-    Antes de responder, evalúa si el mensaje del usuario está relacionado con el plebiscito de la Nueva Constitución de Chile. Si el mensaje trata sobre otro tema, responde educadamente indicando que solo puedes proporcionar información sobre el plebiscito y la propuesta de la Nueva Constitución.
+    Antes de responder, evalúa si el mensaje del usuario está relacionado con el plebiscito de la Nueva Constitución de Chile.
+    Si el mensaje trata sobre otro tema, responde educadamente indicando que
+    solo puedes proporcionar información sobre el plebiscito y la propuesta de la Nueva Constitución.
+   
+    Por ejemplo si te pregunta hola o alguna otra con una constituición, responde:
+    "Hola, solo puedo responder preguntas sobre el plebiscito y la propuesta de la Nueva Constitución.
     
     Mensaje del Usuario:
     {userMessage}
     
-    Evaluación del Mensaje:
-    
-    Si el mensaje está relacionado con el plebiscito o la propuesta de la Nueva Constitución, procede a la sección de respuesta.
-    Si el mensaje trata otros temas, responde con: "Lo siento, solo puedo proporcionar información relacionada con el plebiscito de la Nueva Constitución de Chile de diciembre de 2023."
     Contexto del Documento:
     {documents}
     
     Respuesta en Markdown:
     
-    Si el mensaje es relevante: Responde utilizando la información del documento, incluyendo la referencia del artículo y la página cuando sea posible. Usa Markdown para estructurar tu respuesta de manera clara e interactiva.
-    Si el mensaje no es relevante: "Lo siento, solo puedo proporcionar información relacionada con el plebiscito de la Nueva Constitución de Chile de diciembre de 2023."
   `,
 };
 
